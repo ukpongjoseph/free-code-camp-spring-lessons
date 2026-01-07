a22c18c6983c382d882290843985ddf3233a2684
ukpongjoseph <jukpong499@gmail.com>
2026-01-07 23:25:29 +0100

    Initializing repo

---
+++ src/main/java/dev/joseph_free_code_camp/freecodecamp/FreecodecampApplication.java
@@ -0,0 +1,13 @@
+package dev.joseph_free_code_camp.freecodecamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreecodecampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreecodecampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/joseph_free_code_camp/freecodecamp/FreecodecampApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.joseph_free_code_camp.freecodecamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreecodecampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
