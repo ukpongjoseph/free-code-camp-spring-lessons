cdf767577d2bb5dcb06036f5f3cec1f1d5f83c63
ukpongjoseph <jukpong499@gmail.com>
2026-01-30 02:31:35 +0100

    Mockito testing

--- src/test/java/dev/joseph_free_code_camp/freecodecamp/student/StudentServiceTest.java
+++ src/test/java/dev/joseph_free_code_camp/freecodecamp/student/StudentServiceTest.java
@@ -1,11 +1,18 @@
 package dev.joseph_free_code_camp.freecodecamp.student;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 public class StudentServiceTest {
@@ -24,17 +31,38 @@ public class StudentServiceTest {
 
     @Test
     public void saveStudentTest(){
-        // We plan to test the POST method of creating a new student. From the method in the service class, it can be seen that the POST method recives a StudentDto, converts it to a Student object, saves the Student object and returns a StudentResponseDto
-        // We will first create a test case StudentDto called dto, then perform all necessary opefrations with as seen in the Student service class.
-        StudentDto dto = new StudentDto("Joseph", "Aniekan", "anniejammie49@gmail.com", 1);
-        // Creating a Student object with the dto
-        Student student = studentMapper.dtoToStudent(dto);
-        // Now that we have the Student object, we will generate a StudentResponseDto as response from saving the student details
-        StudentResponseDto studentResponse = studentMapper.studentResponseDto(studentRepository.save(student));
-        // Now that we have all expected values such as the responseDto and the studentObject gotten frm the dto, we will now create our actual values of type Student and StudentResponse Dto
-        Student expectedStudent = new Student(dto.firstname(), dto.lastname(), 25, dto.email());
-        StudentResponseDto expecStudentResponseDto = new StudentResponseDto(dto.firstname(), dto.lastname(), dto.email());
-        // Now that both expected and actual are present, we will test and compare
-        assertEquals(expectedStudent.getFirstname(), student.getFirstname());
+        // We plan to test the POST method of creating a new student. From the method in the service class, it can be seen that the POST method recives a StudentDto, converts it to a Student object, saves the Student object and returns a StudentResponseDto. We would create a mock and a real responseDto and then compare
+        StudentDto dto = new StudentDto("Joseph", "Aniekan", "anie@gmail.com", 1);
+        // Student student = new Student("Joseph", "Aniekan", 24, "anie@gmail.com");
+        Student student1 = new Student("Joseph", "Aniekan", 24, "anie@gmail.com");
+        
+        // Creating mock responseDto
+        // first is taking the dto object and converting it to a student object
+        Mockito.when(studentMapper.dtoToStudent(dto)).thenReturn(student1);
+        // taking the student object and saving it in the db,this method returns a student object
+        Mockito.when(studentRepository.save(student1)).thenReturn(student1);
+        // returning a mock responseDto from the saved student object
+        Mockito.when(studentMapper.studentResponseDto(student1)).thenReturn(new StudentResponseDto("Joseph", "Aniekan", "anie@gmail.com"));
+        
+        // Real responseDto
+        StudentResponseDto responseDto = studentService.saveStudent(dto);
+        assertEquals(dto.firstname(), responseDto.firstname());
+
+        verify(studentMapper, (times(1))).studentResponseDto(student1);
+        verify(studentMapper, times(1)).dtoToStudent(dto);
+        verify(studentRepository, times(1)).save(student1);
+
+    }
+
+    @Test
+    public void findAllStudentsTest(){
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Joseph", "Aniekan", 24, "anie@gmail.com"));
+        Mockito.when(studentRepository.findAll()).thenReturn(students);
+        Mockito.when(studentMapper.studentResponseDto(any(Student.class))).thenReturn(new StudentResponseDto("Joseph", "Aniekan", "anie@gmail.com"));
+
+        List<StudentResponseDto> studentResponseDtos = studentService.fetchAllStudents();
+
+        assertEquals(students.size(), studentResponseDtos.size());
     }
 }
